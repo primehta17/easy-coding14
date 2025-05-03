7e095b482eefcfb78ccf374d775df48e22ed3320
Priyanka Mehta <primehta17@gmail.com>
2025-05-03 23:57:50 +0530

    Stalactite or Stalagmite

---
+++ five.js
@@ -0,0 +1,14 @@
+// write a function to determine ifa ggiven structure is Stalactite or Stalactite
+
+function identifyStructure(structure) {
+    for (let i = 0; i < structure.length; i++) {
+        if (structure[0] == 'c') {
+            return "Stalactite";
+        } else if (structure[0] == 'f') {
+            return "Stalagmite"
+        } else {
+            return "Neither";
+        }
+    }
+}
+console.log(identifyStructure("cammy"))
